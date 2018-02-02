2cb242b36537032b065311bd2c05a73683a014d0
songyewei <syw12345ht@gmail.com>
2018-02-02 14:58:29 +0800

    word2vec

---
+++ Word2vec/word2vec.py
@@ -0,0 +1,9 @@
+#coding:utf-8
+from gensim.models import word2vec
+import logging
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+sentences = word2vec.Text8Corpus('text8')
+model = word2vec.Word2Vec(sentences, size=200)
+
+print(model.similarity('dogs','sleep'))
