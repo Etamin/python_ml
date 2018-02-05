e8420dd77c07a8c6a1351a0136e3bd63e8c98a20
songyewei <syw12345ht@gmail.com>
2018-02-05 14:58:53 +0800

    hmm

--- RandomForest/randomforest.py
+++ RandomForest/randomforest.py
@@ -1,9 +1,16 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import load_iris
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import cross_val_score
 
-clf = RandomForestClassifier(n_estimators=10)
-iris = load_iris()
-clf.fit(iris.data, iris.target)
+#random forest
+clf = RandomForestClassifier(n_estimators=10, max_depth=None,min_samples_split=2, random_state=0)
+X, Y = make_blobs(n_samples=10000, n_features=10, centers=100,random_state=0)
+scores=cross_val_score(clf,X,Y)
+print scores.mean()
 
-predict=clf.predict([[0.1,0.2,0.3,0.4]])
-print(predict)
+#extra tree
+clf = ExtraTreesClassifier(n_estimators=10, max_depth=None,min_samples_split=2, random_state=0)
+X, Y = make_blobs(n_samples=10000, n_features=10, centers=100,random_state=0)
+scores=cross_val_score(clf,X,Y)
+print scores.mean()
---
+++ hmm/hmm.py
@@ -0,0 +1,33 @@
+import math
+import numpy as np
+from hmmlearn import hmm
+
+states = ["box 1", "box 2", "box3"]
+n_states = len(states)
+
+observations = ["red", "white"]
+n_observations = len(observations)
+
+start_probability = np.array([0.2, 0.4, 0.4])
+
+transition_probability = np.array([
+  [0.5, 0.2, 0.3],
+  [0.3, 0.5, 0.2],
+  [0.2, 0.3, 0.5]
+])
+
+emission_probability = np.array([
+  [0.5, 0.5],
+  [0.4, 0.6],
+  [0.7, 0.3]
+])
+
+model = hmm.MultinomialHMM(n_components=n_states)
+model.startprob_=start_probability
+model.transmat_=transition_probability
+model.emissionprob_=emission_probability
+
+
+seen = np.array([[0,0,1]]).T
+hidden_states = model.predict(seen)
+print math.exp(model.score(seen))
